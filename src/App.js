da0b9c6190efc2c2d129936f09e80b5b393c1fd6
Augusto Costa <augustogiles@gmail.com>
2020-01-19 13:22:55 +0000

    add styled-components and open sans as global font

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Open Sans', sans-serif;
+  }
+`
 
 function App() {
   return (
-    <div>
-      Hello Sliceline
-    </div>
+    <>
+      <GlobalStyle/>
+      <div>
+        Hello Sliceline
+      </div>
+    </>
   );
 }
 
