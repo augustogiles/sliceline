1196956d6a9dd821d8f4faeb8450c6a316d79257
Augusto Costa <augustogiles@gmail.com>
2020-01-24 23:44:21 +0000

    adding css transitions on hover

--- src/Menu/FoodGrid.js
+++ src/Menu/FoodGrid.js
@@ -22,5 +22,15 @@ export const Food = styled(Title) `
   background-size: cover;
   filter: contrast(75%);
   border-radius: 7px;
-  box-shadow: 0px 0px 10px 0px grey;
+  margin-top: 5px;
+  transition-property: box-shadow margin-top filter;
+  -o-transition-duration: .1s;
+  box-shadow: 0px 0px 2px 0px grey;
+  &:hover {
+    cursor: pointer;
+    filter: contrast(100%);
+    margin-top: 0px;
+    margin-bottom: 5px;
+    box-shadow: 0px 0px 10px 0px grey;
+  }
 `;
