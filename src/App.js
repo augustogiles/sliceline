d9a915675957408b52d44d0750098e925cc744f9
Augusto Costa <augustogiles@gmail.com>
2020-03-19 17:54:40 +0000

    adding react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import Navbar from './Navbar/Navbar';
-import { Banner } from './Banner/Banner';
-import Menu from './Menu/Menu';
 import { GlobalStyle } from './Styles/GlobalStyle';
 import Order from './Order/Order';
-import FoodDialog from './FoodDialog/FoodDialog';
 import useOpenFood from './Hooks/useOpenFood';
 import useOrders from './Hooks/useOrders';
 import useTitle from './Hooks/useTitle';
 import useAuth from './Hooks/useAuth';
-import OrderDialog from './Order/OrderDialog';
 import useOrderDialog from './Hooks/useOrderDialog';
+import Routes from './routes';
 
 function App() {
   const openFood = useOpenFood();
@@ -22,12 +19,9 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <OrderDialog {...orderDialog} {...orders} />
-      <FoodDialog {...openFood} {...orders} />
       <Navbar {...auth} />
       <Order {...orders} {...openFood} {...auth} {...orderDialog} />
-      <Banner />
-      <Menu {...openFood} />
+      <Routes />
     </>
   );
 }
