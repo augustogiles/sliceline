ce522b87d6d1cb4c77ddb71987b0dda656e56bc9
Augusto Costa <augustogiles@gmail.com>
2020-03-28 22:23:51 +0000

    fetching foods from firebase

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -1,12 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ContentWrapper } from '../Styles/commons';
-import { foods, formatPrice } from '../Data/FoodData';
+import { formatPrice } from '../Data/FoodData';
 import { Food, FoodGrid, FoodLabel } from './FoodGrid';
+import { database } from '../firebase';
+
+function reduceItem(foodList) {
+  return foodList.reduce((res, food) => {
+    if (!res[food.section]) {
+      res[food.section] = [];
+    }
+
+    res[food.section].push(food);
+    return res;
+  }, {});
+}
+
+async function getFood() {
+  let foodList = null;
+  const ordersListRef = database.ref('foods');
+
+  await ordersListRef.once(await 'value', snap => {
+    const objects = snap.val();
+    foodList = Object.keys(snap.val()).map(id => {
+      const data = objects[id];
+      const food = {
+        id,
+        ...data
+      };
+
+      return food;
+    });
+  });
+
+  return reduceItem(foodList);
+}
 
 function Menu({ setOpenFood }) {
+  const [foodList, setFoodList] = useState([]);
+
+  useEffect(() => {
+    getFood()
+      .then(list => {
+        setFoodList(list);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <ContentWrapper>
-      {Object.entries(foods).map(([sectionName, section]) => (
+      {Object.entries(foodList).map(([sectionName, section]) => (
         <React.Fragment key={`frag${sectionName}`}>
           <h1 key={`section_${sectionName}`}>{sectionName}</h1>
           <FoodGrid key={sectionName}>
