953b8955fa0a852a31df9dfb5bf8a6f50b18e29e
Augusto Giles <augustogiles@gmail.com>
2020-03-29 23:42:35 +0100

    Shopping cart (#9)
    * fix logout
    
    * adding custom hooks to open cart
    
    * adding cart
    
    * removing commented code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 import { BrowserRouter } from 'react-router-dom';
 import Banner from './Banner/Banner';
 import Navbar from './Navbar/Navbar';
 import GlobalStyle from './Styles/GlobalStyle';
 import Order from './Order/Order';
 import useOpenFood from './Hooks/useOpenFood';
+import useOpenCart from './Hooks/useOpenCart';
 import useOrders from './Hooks/useOrders';
 import useTitle from './Hooks/useTitle';
 import useAuth from './Hooks/useAuth';
 import useOrderDialog from './Hooks/useOrderDialog';
 import Routes from './routes';
 
+const Container = styled.div`
+  margin-right: ${props => (props.openCart ? '340px' : '0')};
+  transition: margin-right 0.5s ease-in-out;
+`;
+
 function App() {
+  const openCart = useOpenCart();
   const openFood = useOpenFood();
   const orders = useOrders();
   const auth = useAuth();
@@ -22,16 +30,22 @@ function App() {
     <>
       <BrowserRouter>
         <GlobalStyle />
-        <div style={{ paddingRight: '340px' }}>
+        <Container openCart={openCart.openCart}>
           <Banner />
-          <Navbar {...auth} />
-          <Order {...orders} {...openFood} {...auth} {...orderDialog} />
+          <Navbar {...auth} {...openCart} />
+          <Order
+            {...orders}
+            {...openFood}
+            {...auth}
+            {...orderDialog}
+            {...openCart}
+          />
           <Routes
             orders={orders}
             orderDialog={orderDialog}
             openFood={openFood}
           />
-        </div>
+        </Container>
       </BrowserRouter>
     </>
   );
