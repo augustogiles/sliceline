ee51edc58c48890c50c159e3b7336cb26444c566
Augusto Costa <augustogiles@gmail.com>
2020-03-30 22:54:13 +0100

    fix some style in the contentWrapper

--- src/Styles/commons.js
+++ src/Styles/commons.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const ContentWrapper = styled.div`
   height: 1000px;
   padding: 40px;
-  margin: 0 0 50px 20px;
 `;
 
 export const Title = styled.div`
