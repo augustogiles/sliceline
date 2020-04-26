c9e371d5e92470a77b26c19fa2a92a778da3d500
Augusto Costa <augustogiles@gmail.com>
2020-04-26 01:59:36 +0100

    updating transition animation in food grid

--- src/components/Menu/FoodGrid.js
+++ src/components/Menu/FoodGrid.js
@@ -24,14 +24,15 @@ export const Food = styled.div`
   filter: contrast(75%);
   border-radius: 7px;
   margin-top: 5px;
-  transition-property: box-shadow margin-top filter;
-  -o-transition-duration: 0.1s;
-  box-shadow: 0px 0px 2px 0px grey;
+  box-shadow: 0px 2px 10px 0px rgba(0, 0, 0, 0.2);
+
+  transition: all 0.2s ease-in-out;
+  -o-transition-duration: 0.2s;
   &:hover {
     cursor: pointer;
     filter: contrast(100%);
     margin-top: 0px;
     margin-bottom: 5px;
-    box-shadow: 0px 0px 10px 0px grey;
+    box-shadow: 0px 2px 10px 0px rgba(0, 0, 0, 0.4);
   }
 `;
